a9c8d97dbb217ce5e77bda742cc9e5530d29715b
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-10 05:35:18 +0200

    добавляет структуру

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+import "./App.css";
+// import { v4 as uuidv4 } from "uuid";
+// uuidv4();
+class App extends Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+  handleChange = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setState({ name: e.target.value });
+
+    console.log(this.state);
+    console.log(`ввели имя:`);
+  };
+  render() {
+    const { name, contacts } = this.state;
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            <input
+              type="text"
+              placeholder="Enter name"
+              name="name"
+              onChange={this.handleChange}
+              value={name}
+            ></input>
+          </label>
+        </form>
+        {/* <ContactForm /> */}
+
+        <h2>Contacts</h2>
+        {/* <Filter />
+  <ContactList /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
